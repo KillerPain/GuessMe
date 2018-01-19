2351c2e24a6d4d0f67ec1b868993715465a63baa
Ильяс Сабешулы <emailforanypurposes@gmail.com>
2018-01-20 05:28:55 +0600

    added sort and score to cards with players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,7 +11,9 @@ import { Router } from '@angular/router';
 export class GameComponent implements OnInit {
 
   id: number;
-  
+  players = [{ name: 'Ilias', score: 50 }, { name: 'Ilias', score: 60 }, { name: 'Ilias', score: 10 },
+  { name: 'Ilias', score: 40 }, { name: 'Ilias', score: 30 }, { name: 'Ilias', score: 80 }];
+
   constructor(private route: ActivatedRoute, private r: Router, private service: GameComponentService) { }
 
   ngOnInit() {
@@ -19,15 +21,18 @@ export class GameComponent implements OnInit {
       this.id = +params['id'];
       const playerData = JSON.parse(localStorage.getItem('playerData'));
       if (playerData === null || playerData['token'] === null) {
-        this.toJoin(this.id);
+        //this.toJoin(this.id);
         return;
       }
       this.service.hasPlayer(this.id, playerData).then(result => {
         console.log(result);
         if (!result) {
-          this.toJoin(this.id);
+          // this.toJoin(this.id);
         }
-      })
+      });
+    });
+    this.players.sort((obj1, obj2) => {
+      return obj2.score - obj1.score;
     });
   }
 
